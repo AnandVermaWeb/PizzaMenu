1d03149d53f937555d59a6d16670abef5ac06ce5
AnandVermaWeb <anandverma2072001@gmail.com>
2024-08-28 22:56:39 +0530

    pizza list display

---
+++ src/pages/pizza/pizza.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import pizzadatas from '../pizza/pizzaData/pizzadatas'
+ const Pizza = () => {
+  console.log(pizzadatas)
+  return (
+    <div>
+      { pizzadatas && pizzadatas?.map((pizza:any, index:any) => (
+        <div key={index}>
+          <h2>{pizza.name}</h2>
+          <p>{pizza.ingredients}</p>
+          <p>Price: ${pizza.price}</p>
+          {!pizza.soldOut ? (
+            <img src={pizza.photoName} alt={pizza.name} />
+          ) : (
+            <p>Sold Out</p>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+}
+export default Pizza
